2043853b5b1e1c1d61ca834dbcedec2345008c2c
lcrispo <no>
2014-07-21 10:04:48 -0700

    Ok

---
+++ WebAtCloudScale/Helpers/CDNHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace WebAtScale.Helpers
+{
+    public static class CDNHelper
+    {
+        public static string CDN(this UrlHelper helper, string target)
+        {
+            string CDNTarget = "";
+            if (WebConfigurationManager.AppSettings["BlobStore"] == "CDN") 
+            {
+                Uri URITarget = new Uri(target);
+                CDNTarget = WebConfigurationManager.AppSettings["CDNEndpoint"] + URITarget.AbsolutePath;
+            }
+            else 
+            {
+                CDNTarget = helper.Content(target);
+            }
+            return  CDNTarget;
+
+        }
+
+   
+    }
+}
---
+++ WebAtCloudScale/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebAtScale")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebAtScale")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("de4481e3-fad9-4ee9-a4e3-f33ebd49018d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
